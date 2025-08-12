8e682def37d95397d3448b5ce2572976ecbe0ab2
unknown <kikialex37@gmail.com>
2025-08-12 21:13:06 +0300

    Designing a Database Table in Django

--- documents/models.py
+++ documents/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+
+User = settings.AUTH_USER_MODEL
+
+class Document(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(default="Title")
+    content = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
